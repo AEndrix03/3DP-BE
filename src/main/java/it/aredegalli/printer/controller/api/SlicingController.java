53b6f46d5415691697569917fd062314900a8de8
AEndrix <redegalli.andrea03@gmail.com>
2025-06-17 01:33:13 +0200

    chore: cambio parametri slicing endpoints

--- src/main/java/it/aredegalli/printer/controller/api/SlicingController.java
+++ src/main/java/it/aredegalli/printer/controller/api/SlicingController.java
@@ -3,6 +3,7 @@ package it.aredegalli.printer.controller.api;
 import it.aredegalli.printer.dto.slicing.SlicingResultDto;
 import it.aredegalli.printer.service.log.LogService;
 import it.aredegalli.printer.service.slicing.SlicingService;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,20 +19,20 @@ public class SlicingController {
     private final SlicingService slicingService;
     private final LogService log;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<SlicingResultDto> getSlicingResultById(@PathVariable UUID id) {
+    @GetMapping()
+    public ResponseEntity<SlicingResultDto> getSlicingResultById(@RequestParam("id") @NotNull UUID id) {
         log.info("SlicingController", "getSlicingResultById with id: " + id);
         return ResponseEntity.ok(slicingService.getSlicingResultById(id));
     }
 
-    @GetMapping("/source/{sourceId}")
-    public ResponseEntity<List<SlicingResultDto>> getAllSlicingResultBySourceId(@PathVariable UUID sourceId) {
-        log.info("SlicingController", "getAllSlicingResultBySourceId with source id: " + sourceId);
-        return ResponseEntity.ok(slicingService.getAllSlicingResultBySourceId(sourceId));
+    @GetMapping("/source")
+    public ResponseEntity<List<SlicingResultDto>> getAllSlicingResultBySourceId(@RequestParam("id") @NotNull UUID id) {
+        log.info("SlicingController", "getAllSlicingResultBySourceId with source id: " + id);
+        return ResponseEntity.ok(slicingService.getAllSlicingResultBySourceId(id));
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteSlicingResultById(@PathVariable UUID id) {
+    @DeleteMapping()
+    public ResponseEntity<Void> deleteSlicingResultById(@RequestParam("id") @NotNull UUID id) {
         log.info("SlicingController", "deleteSlicingResultById with id: " + id);
         slicingService.deleteSlicingResultById(id);
         return ResponseEntity.noContent().build();
